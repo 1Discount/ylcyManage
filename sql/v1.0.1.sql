ab28482f8b9faab766778213ab348e6353590caf
wuyue <wye@putao.cn>
2017-12-12 22:43:51 +0800

    sql and jdbc

---
+++ sql/v1.0.1.sql
@@ -0,0 +1,5 @@
+-- 运营监控中的“异常监控“去掉
+DELETE FROM roletomenu  WHERE `menuID` = 'a724bad0-9ef8-4672-a217-c6c2dc92bdec';
+
+-- SIM服务器维护—》卡池服务器维护
+UPDATE menugroupinfo mg SET mg.`MenuGroupName` = '卡池服务器维护' WHERE mg.`MenuGroupName` = 'SIM服务器维护';
